f25dd9efcca1ea63ef402de4a7fe590c66e85293
yuki <yuki.laurentiu@gmail.com>
2025-01-07 12:23:24 +0900

    added function and props

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
 import './App.css';
-
-function ProductCard() {
-  return (
-    <article>
-      <h2>iPhone 15 Pro</h2>
-      <img
-        src="images/iphone.png"
-        alt="iPhone 15 Pro"
-        width="128px"
-        height="128px"
-      />
-      <p>Specification:</p>
-      <ul>
-        <li>A17 Pro chip with 6-core GPU</li>
-        <li>3x or 5x Telephoto camera</li>
-        <li>Up to 29 hours video playback</li>
-      </ul>
-      <button>Buy (From $999)</button>
-    </article>
-  )
-}
+import {ProductCard } from './components/ProductCard';
+import { ProductList } from './components/ProductList';
 
 function App() {
+  const product = {
+    imageSrc: "images/iphone.png",
+    title: "iPhone 15 Pro",
+    specification: [
+      "A17 Pro chip with 6-core GPU",
+      "3x or 5x Telephoto camera",
+      "Up to 29 hours video playback",
+    ],
+    price: 999,
+  };
   return (
     <div className="App">
-      <ProductCard />
+      <ProductList>
+        <ProductCard product={product}/>
+        <ProductCard product={product}/>
+        <ProductCard product={product}/>
+      </ProductList>
     </div>
   );
 }
